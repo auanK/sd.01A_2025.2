b34bd9750a53bfee086fd265ac1dbc8d98c6ac7b
Kauan Pablo <kauanpablo089@gmail.com>
2026-01-14 23:28:01 -0300

    Upload lab FastAPI

---
+++ FastAPI/config.py
@@ -0,0 +1,13 @@
+import configparser
+
+# Cria um objeto ConfigParser
+config = configparser.ConfigParser()
+
+# Adiciona as propriedades (chave-valor)
+config['DEFAULT'] = {
+    'arquivo': 'estoque.xml',
+}
+
+# Tenta gravar as propriedades no arquivo "config.ini"
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
